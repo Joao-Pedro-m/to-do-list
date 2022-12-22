d4b0b206951b85abdf7ccada85badf3686c4e322
Joao-Pedro-m <joaopedromonteiroferreira@gmail.com>
2022-12-22 19:18:47 -0400

    estilos integrados com a aplicação

--- to-do-list-nextjs/components/navbar.js
+++ to-do-list-nextjs/components/navbar.js
@@ -3,14 +3,14 @@ import Link from "next/link"
 export default function Navbar(){
 	return (
 		<>
-			<ul>
-				<li>
+			<ul className="ul-navbar">
+				<li className="li-navbar">
 					<Link href="/">Home</Link>
 				</li>
-				<li>
+				<li className="li-navbar">
 					<Link href="/login">Log in</Link>
 				</li>
-				<li>
+				<li className="li-navbar">
 					<Link href="/signup">Sign up</Link>
 				</li>
 			</ul>
--- to-do-list-nextjs/pages/_app.js
+++ to-do-list-nextjs/pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from '../components/layout'
+import "../styles/globals.css"
 
 export default function MyApp({ Component, pageProps }) {
   return (
