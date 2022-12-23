d6e0d82042196838d6d444cc58db91d40b138ff1
Joao-Pedro-m <joaopedromonteiroferreira@gmail.com>
2022-12-23 03:33:28 -0400

    navbar pronto

--- to-do-list-nextjs/components/navbar.js
+++ to-do-list-nextjs/components/navbar.js
@@ -3,20 +3,21 @@ import Link from "next/link"
 export default function Navbar(){
 	return (
 		<div className="navbar">
-			<ul className="ul-navbar">
-				<li className="li-navbar">
-					<a href="/" className="link-navbar">Home</a>
-				</li>
-				<li className="li-navbar">
-					<a href="/login" className="link-navbar">Log in</a>
-				</li>
-				<li className="li-navbar">
-					<a href="/signup" className="link-navbar">Sign up</a>
-				</li>
-			</ul>
 			<div className="title-navbar">
 				To-Do App
 			</div>
+			<ul className="ul-navbar">
+		        	<li className="li-navbar">
+		                	<a href="/" className="link-navbar">Home</a>
+		                </li>
+		                <li className="li-navbar">
+		                	<a href="/login" className="link-navbar">Log in</a>
+		                </li>
+		                <li className="li-navbar">
+		                	<a href="/signup" className="link-navbar">Sign up</a>
+		                </li>
+			</ul>
+
 		</div>
   	)
 }
